6ccb5e1f2fc8a62f6e5a86330ec56f86dba20521
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-27 16:40:13 +0700

    Kết nối mysql và project

---
+++ config/database.ts
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv" ;
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST, // link hosting
+    dialect: 'mysql'
+  }
+);
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.error('Unable to connect to the database: ', error);
+});
+
+export default sequelize ;
--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import express,{ Express , Request , Response } from "express";
+import sequelize from "./config/database";
+import dotenv from "dotenv" ;
+dotenv.config();
 const app : Express = express() ;
-const port:Number = 3000;
+const port: number | string = process.env.PORT || 3000;
 
 app.set('views' , `${__dirname}/views`);
 app.set('view engine' , 'pug');
+sequelize;
 
 app.get("/tours" , (req:Request , res : Response) => {
     res.render("client/pages/tours/index.pug");

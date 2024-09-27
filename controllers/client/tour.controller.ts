1583c85e3355e539d0a0008caee122e83d4c745a
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-27 22:42:01 +0700

    Làm trang danh sách tour theo danh mục

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,15 +1,35 @@
+import sequelize from "../../config/database";
 import Tour from "../../models/tour.model";
 import { Request , Response } from "express";
+import { QueryTypes } from "sequelize";
 
+// GET /tours/:slugCategory
 export const index = async (req:Request , res : Response) => {
-    const tours = await Tour.findAll({
-        where: {
-            deleted :false ,
-            status : "active"
-        },
-        raw : true 
-    })
-    console.log(tours);
+    const slugCategory = req.params.slugCategory ;
+
+    const tours = await sequelize.query(`
+        SELECT tours.*,ROUND(price * (1 - discount/100)) AS price_special
+        FROM tours
+        JOIN tours_categories ON tours.id = tours_categories.tour_id
+        JOIN categories ON tours_categories.category_id = categories.id
+        WHERE 
+            categories.slug = '${slugCategory}'
+            AND categories.deleted = false
+            AND categories.status = 'active'
+            AND tours.deleted = false
+            AND tours.status = 'active' ;
+    `,{
+        type : QueryTypes.SELECT // Phải định nghĩa kiểu query để không bị lồng mảng vào
+    });
+    
+
+    tours.forEach(item => {
+        if(item["images"]){
+            const images = JSON.parse(item["images"]);
+            item["image"] = images[0];
+        }
+    }) ;
+    
     
     res.render("client/pages/tours/index.pug" , {
         tours : tours
--- router/client/tour.route.ts
+++ router/client/tour.route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import * as controller from "../../controllers/client/tour.controller";
 const router: Router = Router();
 
-router.get("/", controller.index);
+router.get("/:slugCategory", controller.index);
 
 
 export const tourRoutes: Router = router;

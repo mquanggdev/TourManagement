fde561a506a847144b2bb8eb75f25db30c1f21b8
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-27 17:15:33 +0700

    Tạo model và lấy ra dữ liệu đầu tiên

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express,{ Express , Request , Response } from "express";
 import sequelize from "./config/database";
 import dotenv from "dotenv" ;
+import Tour from "./models/tour.model";
 dotenv.config();
 const app : Express = express() ;
 const port: number | string = process.env.PORT || 3000;
@@ -9,8 +10,15 @@ app.set('views' , `${__dirname}/views`);
 app.set('view engine' , 'pug');
 sequelize;
 
-app.get("/tours" , (req:Request , res : Response) => {
-    res.render("client/pages/tours/index.pug");
+app.get("/tours" , async (req:Request , res : Response) => {
+    const tours = await Tour.findAll({
+        raw : true
+    })
+    console.log(tours);
+    
+    res.render("client/pages/tours/index.pug" , {
+        tours : tours
+    });
 })
 
 app.listen(port, () => {

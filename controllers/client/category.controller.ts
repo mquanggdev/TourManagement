1e6b77d5bbae5fac4f5d3bc0b4ced9d5a125b6f7
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-27 18:50:29 +0700

    Làm trang danh mục bên client

--- controllers/client/category.controller.ts
+++ controllers/client/category.controller.ts
@@ -1,8 +1,17 @@
-import Tour from "../../models/tour.model";
+import Category from "../../models/category.model";
 import { Request , Response } from "express";
 
 export const index = async (req:Request , res : Response) => {
+    const categories = await Category.findAll({
+        where: {
+            deleted :false ,
+            status : "active"
+        },
+        raw : true 
+    })
+    console.log(categories);
     res.render("client/pages/categories/index.pug" , {
-        pageTitle : "Trang Danh Mục"
+        pageTitle : "Trang Danh Mục" ,
+        categories : categories
     });
 }

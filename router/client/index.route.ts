279bf482f490ecd60c287c77fdf98752f7c7a6bb
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-10-08 11:00:46 +0700

    Lấy thông tin đơn hàng gửi lên sever

--- router/client/index.route.ts
+++ router/client/index.route.ts
@@ -2,12 +2,13 @@ import { Express } from "express";
 import { tourRoutes } from "./tour.route";
 import { categoryRoutes } from "./category.route";
 import { cartRoute } from "./cart.route";
+import { orderRoute } from "./order.route";
 const clientRoutes = (app: Express): void => {
 
   app.use(`/tours`, tourRoutes);
   app.use(`/categories`, categoryRoutes );
   app.use("/cart", cartRoute);
-  
+  app.use("/order", orderRoute);
 };
 
 export default clientRoutes;
---
+++ router/client/order.route.ts
@@ -0,0 +1,5 @@
+import express from "express";
+const router = express.Router();
+import * as controller from "../../controllers/client/order.controller";
+router.post("/", controller.index);
+export const orderRoute = router;

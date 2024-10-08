7b4c38ffba4241a4110098bb941498a13d17a0a1
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-10-08 16:43:31 +0700

    Làm trang đặt hàng thành công

--- router/client/order.route.ts
+++ router/client/order.route.ts
@@ -2,4 +2,5 @@ import express from "express";
 const router = express.Router();
 import * as controller from "../../controllers/client/order.controller";
 router.post("/", controller.index);
+router.get("/success/:orderCode", controller.success);
 export const orderRoute = router;

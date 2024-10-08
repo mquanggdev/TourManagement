9bf6fed4187acd297a6ddd6986e698699bbf3e88
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-10-08 10:42:42 +0700

    lấy data từ database và in ra giao diện

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express,{ Express } from "express";
 import dotenv from "dotenv" ;
 import clientRoutes from "./router/client/index.route";
+import bodyParser from "body-parser";
+
 dotenv.config();
 const app : Express = express() ;
 const port: number | string = process.env.PORT || 3000;
@@ -8,6 +10,8 @@ const port: number | string = process.env.PORT || 3000;
 app.set('views' , `${__dirname}/views`);
 app.set('view engine' , 'pug');
 app.use(express.static(`${__dirname}/public`));
+// parse application/json
+app.use(bodyParser.json());
 
 // Client route
 clientRoutes(app);
--- router/client/cart.route.ts
+++ router/client/cart.route.ts
@@ -2,4 +2,5 @@ import express from "express";
 const router = express.Router();
 import * as controller from "../../controllers/client/cart.controller";
 router.get("/", controller.index);
+router.post("/list-json", controller.listJson);
 export const cartRoute = router;

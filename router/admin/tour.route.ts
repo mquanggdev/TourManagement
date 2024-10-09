9568b9fff791311b38535018363be7e64c007eed
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-10-09 21:09:48 +0700

    upload nhiều ảnh

--- router/admin/tour.route.ts
+++ router/admin/tour.route.ts
@@ -1,8 +1,13 @@
 import express from "express";
 const router = express.Router();
+import multer from "multer" ;
+import { uploadFields } from "../../middlewares/admin/uploadCloud.middleware";
+
+const upload = multer() ;
 
 import * as controller from "../../controllers/admin/tour.controller";
 router.get("/", controller.index);
 router.get("/create" , controller.create);
-router.post("/create" , controller.createPost);
+router.post("/create" ,upload.fields([{name : "images" ,maxCount : 10}]) ,
+uploadFields ,controller.createPost);
 export const tourRoute = router;
